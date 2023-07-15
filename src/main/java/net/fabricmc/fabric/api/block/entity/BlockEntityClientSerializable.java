ccba5f4bfe050777fd6e68bc979f0edadbc72f54
Kasualix <670067575@qq.com>
2023-07-15 09:46:48 +0800

    block entity networking api

--- src/main/java/net/fabricmc/fabric/api/block/entity/BlockEntityClientSerializable.java
+++ src/main/java/net/fabricmc/fabric/api/block/entity/BlockEntityClientSerializable.java
@@ -17,6 +17,7 @@
 package net.fabricmc.fabric.api.block.entity;
 
 import com.google.common.base.Preconditions;
+
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.server.world.ServerWorld;
---
+++ src/main/java/net/fabricmc/fabric/mixin/papi/networing/block_entity/MixinIForgeTileEntity.java
@@ -0,0 +1,32 @@
+package net.fabricmc.fabric.mixin.papi.networing.block_entity;
+
+import net.fabricmc.fabric.api.block.entity.BlockEntityClientSerializable;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraftforge.common.extensions.IForgeEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+
+@Mixin(value = IForgeEntity.class, priority = 0, remap = false)
+public interface MixinIForgeTileEntity {
+    @Shadow
+    default BlockEntity getTileEntity() {
+        return (BlockEntity)this;
+    }
+
+    /**
+     * @author Kasualix
+     * @reason impl fabric block entity networking api
+     **/
+    @Overwrite
+    default void handleUpdateTag(BlockState state, CompoundTag tag) {
+        BlockEntity blockEntity = this.getTileEntity();
+        if (blockEntity instanceof BlockEntityClientSerializable) {
+            ((BlockEntityClientSerializable) blockEntity).fromClientTag(tag);
+        } else {
+            blockEntity.fromTag(state, tag);
+        }
+    }
+}
