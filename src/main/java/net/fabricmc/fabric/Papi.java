9c6b3dc10fe173b5f54ee2d3ab867bad62955adf
Kasualix <670067575@qq.com>
2023-08-11 15:41:57 +0800

    fix build

--- src/main/java/net/fabricmc/fabric/Papi.java
+++ src/main/java/net/fabricmc/fabric/Papi.java
@@ -2,6 +2,7 @@ package net.fabricmc.fabric;
 
 import net.fabricmc.fabric.impl.client.event.lifecycle.ClientLifecycleEventsImpl;
 import net.fabricmc.fabric.impl.event.lifecycle.LifecycleEventsImpl;
+import net.fabricmc.fabric.impl.lookup.ApiLookupImpl;
 import net.fabricmc.fabric.impl.networking.NetworkingImpl;
 import net.fabricmc.fabric.impl.networking.client.ClientNetworkingImpl;
 import net.minecraftforge.common.MinecraftForge;
@@ -17,6 +18,7 @@ public class Papi {
         LifecycleEventsImpl.init();
         MinecraftForge.EVENT_BUS.register(LifecycleEventsImpl.class);
 
+        ApiLookupImpl.init();
         if (FMLLoader.getDist().isClient()) {
             ClientNetworkingImpl.clientInit();
 
