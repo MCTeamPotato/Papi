8e487f649898f773088819a38f318599c5ea0583
Kasualix <670067575@qq.com>
2023-08-21 01:57:59 +0800

    Update FabricLoader.java

--- src/main/java/papi/FabricLoader.java
+++ src/main/java/papi/FabricLoader.java
@@ -1,20 +1,30 @@
 package papi;
 
 import net.minecraft.client.MinecraftClient;
+import net.minecraftforge.server.ServerLifecycleHooks;
 
 import java.nio.file.Path;
 
 public class FabricLoader {
-    private static final FabricLoader INSTANCE = new FabricLoader();
-
     private final Path configDir;
 
     public FabricLoader() {
-        this.configDir = MinecraftClient.getInstance().runDirectory.toPath().resolve("config");
+        this.configDir = getConfigPath();
+    }
+
+    private static Path getConfigPath() {
+        Path path;
+        try {
+            path = MinecraftClient.getInstance().runDirectory.toPath().resolve("config");
+        } catch (Throwable e){
+
+            path = ServerLifecycleHooks.getCurrentServer().getRunDirectory().toPath().resolve("config");
+        }
+        return path;
     }
 
     public static FabricLoader getInstance() {
-        return INSTANCE;
+        return new FabricLoader();
     }
 
     public Path getConfigDir() {
