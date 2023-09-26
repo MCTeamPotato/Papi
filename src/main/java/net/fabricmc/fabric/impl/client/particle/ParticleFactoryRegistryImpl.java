f9aa5fe3e803327e96679d7473e4fab51b0c1103
Kasualix <670067575@qq.com>
2023-09-26 21:37:30 +0800

    particle api

---
+++ src/main/java/net/fabricmc/fabric/impl/client/particle/ParticleFactoryRegistryImpl.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright (c) 2016, 2017, 2018, 2019 FabricMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.fabricmc.fabric.impl.client.particle;
+
+import net.fabricmc.fabric.api.client.particle.v1.FabricSpriteProvider;
+import net.fabricmc.fabric.api.client.particle.v1.ParticleFactoryRegistry;
+import net.minecraft.client.particle.ParticleFactory;
+import net.minecraft.client.particle.ParticleManager;
+import net.minecraft.particle.ParticleEffect;
+import net.minecraft.particle.ParticleType;
+
+import java.util.IdentityHashMap;
+import java.util.Map;
+
+@SuppressWarnings({"deprecation", "rawtypes"})
+public final class ParticleFactoryRegistryImpl implements ParticleFactoryRegistry {
+	public static final ParticleFactoryRegistryImpl INSTANCE = new ParticleFactoryRegistryImpl();
+
+	static class DeferredParticleFactoryRegistry implements ParticleFactoryRegistry {
+		private final Map<ParticleType<?>, ParticleFactory<?>> factories = new IdentityHashMap<>();
+		private final Map<ParticleType<?>, PendingParticleFactory<?>> constructors = new IdentityHashMap<>();
+
+		@Override
+		public <T extends ParticleEffect> void register(ParticleType<T> type, ParticleFactory<T> factory) {
+			factories.put(type, factory);
+		}
+
+		@Override
+		public <T extends ParticleEffect> void register(ParticleType<T> type, PendingParticleFactory<T> factory) {
+			constructors.put(type, factory);
+		}
+
+		@SuppressWarnings("unchecked")
+		void applyTo(ParticleFactoryRegistry registry) {
+			for (Map.Entry<ParticleType<?>, ParticleFactory<?>> entry : factories.entrySet()) {
+				ParticleType type = entry.getKey();
+				ParticleFactory factory = entry.getValue();
+				registry.register(type, factory);
+			}
+
+			for (Map.Entry<ParticleType<?>, PendingParticleFactory<?>> entry : constructors.entrySet()) {
+				ParticleType type = entry.getKey();
+				PendingParticleFactory constructor = entry.getValue();
+				registry.register(type, constructor);
+			}
+		}
+	}
+
+	static class DirectParticleFactoryRegistry implements ParticleFactoryRegistry {
+		private final ParticleManager particleManager;
+
+		DirectParticleFactoryRegistry(ParticleManager particleManager) {
+			this.particleManager = particleManager;
+		}
+
+		@Override
+		public <T extends ParticleEffect> void register(ParticleType<T> type, ParticleFactory<T> factory) {
+			particleManager.registerFactory(type, factory);
+		}
+
+		@Override
+		public <T extends ParticleEffect> void register(ParticleType<T> type, PendingParticleFactory<T> constructor) {
+			particleManager.registerFactory(type, delegate -> {
+				FabricSpriteProvider fabricSpriteProvider = new FabricSpriteProviderImpl(particleManager, delegate);
+				return constructor.create(fabricSpriteProvider);
+			});
+		}
+	}
+
+	ParticleFactoryRegistry internalRegistry = new DeferredParticleFactoryRegistry();
+
+	private ParticleFactoryRegistryImpl() { }
+
+	@Override
+	public <T extends ParticleEffect> void register(ParticleType<T> type, ParticleFactory<T> factory) {
+		internalRegistry.register(type, factory);
+	}
+
+	@Override
+	public <T extends ParticleEffect> void register(ParticleType<T> type, PendingParticleFactory<T> constructor) {
+		internalRegistry.register(type, constructor);
+	}
+
+	public void initialize(ParticleManager particleManager) {
+		ParticleFactoryRegistry newRegistry = new DirectParticleFactoryRegistry(particleManager);
+		DeferredParticleFactoryRegistry oldRegistry = (DeferredParticleFactoryRegistry) internalRegistry;
+		oldRegistry.applyTo(newRegistry);
+		internalRegistry = newRegistry;
+	}
+}
