d30de4fb022da58f67884c639737daa2ebd7b897
Kasualix <670067575@qq.com>
2023-09-28 19:10:34 +0800

    Update ForgeHooksClientMixin.java

--- src/main/java/net/fabricmc/fabric/mixin/screen/ForgeHooksClientMixin.java
+++ src/main/java/net/fabricmc/fabric/mixin/screen/ForgeHooksClientMixin.java
@@ -10,7 +10,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(ForgeHooksClient.class)
+@Mixin(value = ForgeHooksClient.class, remap = false)
 public abstract class ForgeHooksClientMixin {
     @Unique
     private static Screen renderingScreen;
